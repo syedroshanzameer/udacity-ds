c65a9b483d0f1f7dfe2327b5d5747aacdfdcbf50
Roshan Syed <syedroshanzameer@gmail.com>
2021-09-13 19:49:37 -0500

    corrected the load_data function with correct file name for categories_filepath

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,45 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    """ Loading the messages & categories file from the data folder using pandas read_csv"""
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories,on='id',how='outer')
+    return df
+    
 
 def clean_data(df):
-    pass
+    # print(df.head())
+    categories = df['categories'].str.split(';',expand=True)
 
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = []
+    for col in row:
+        category_colnames.append(col.split('-')[0])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = [row[-1] for row in categories[column]]
+        # convert column from string to numeric
+        categories[column] = [int(row) for row in categories[column]]
+    df.drop(['categories'],axis = 1, inplace= True)
+    df = pd.concat([df,categories],axis = 1)
+    df.drop_duplicates(inplace = True)
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('clean_table', engine, index=False)
 
 
 def main():
